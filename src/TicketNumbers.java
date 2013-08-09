f56edd01b7ba3b01691d9b371f5fa94bb6efed4e
clydewbrown <clydewbrown@gmail.com>
2013-08-09 10:36:16 -0400

    Adding java file for students

---
+++ src/TicketNumbers.java
@@ -0,0 +1,12 @@
+
+public class TicketNumbers {
+
+	public static void main(String[] args) {
+		// -------INSTRUCTIONS-------
+		// 1. Copy and paste your code for the ticket numbers problem here (from yesterday)
+		// 2. Then delete these comments
+		// 3. Finally, commit and push the project containing this file to GitHub
+
+	}
+
+}
